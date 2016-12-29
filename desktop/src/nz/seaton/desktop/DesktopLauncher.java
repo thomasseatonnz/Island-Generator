9787cdddc25ad207f288d49cf3dc5bd45921fa4b
Thomas Seaton <thomasseaton@outlook.com>
2016-12-29 19:12:24 +1300

    First Commit - Created libGDX Project

---
+++ desktop/src/nz/seaton/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nz.seaton.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nz.seaton.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
