c06b2ea1254c8fa97f2e0a20f8e171bdc0d63ec7
Thomas Seaton <thomasseaton@outlook.com>
2017-01-25 19:33:54 +1300

    Added archipelago islands

--- core/src/nz/seaton/islandgenerator/island/ArchipelagoTemplate.java
+++ core/src/nz/seaton/islandgenerator/island/ArchipelagoTemplate.java
@@ -17,19 +17,26 @@ public class ArchipelagoTemplate extends IslandTemplate {
 		for (int x = 0; x < w; x++) {
 			for (int y = 0; y < h; y++) {
 				template[x][y] = -0.3;
-				
-				double cRad = Vector2.dst(x, y, w/2, h/2);
-				
-				int islandStartRad = radius - thickness;
-				int islandEndRad = radius + thickness;
-				double distFromIslandStart = (cRad - (double)islandStartRad);
-				
-				if(cRad > islandStartRad && cRad < islandEndRad){
-					template[x][y] += ((-0.5)*Math.cos((distFromIslandStart/(thickness/3)))) + 0.5;
-					template[x][y] *= 0.2;
-				}
+
+				double cRad = Vector2.dst(x, y, w / 2, h / 2);
+
+				float amplitude = 0.5f;
+				float offset = 1.0f;
+
+				template[x][y] -= (smoothstep(radius, radius + thickness, (float) cRad) * amplitude) - offset;
+				template[x][y] += (smoothstep(radius - thickness, radius, (float) cRad) * amplitude) - offset;
+				template[x][y] -= (smoothstep(radius + thickness, 3 * radius, (float) cRad));
 			}
 		}
+	}
+
+	private static float smoothstep(float edge0, float edge1, float x) {
+		x = clamp((x - edge0) / (edge1 - edge0), 0.0f, 1.0f);
+		return x * x * (3 - 2 * x);
+	}
+
+	private static float clamp(float value, float min, float max) {
+		return (value < min) ? min : (value > max) ? max : value;
 	}
 
 }
